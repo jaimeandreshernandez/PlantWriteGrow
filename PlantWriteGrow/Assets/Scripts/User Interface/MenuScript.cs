44ee8f095bdbc855b9669143b0a25607b987c904
Eric-Z0 <ericzhuang603@gmail.com>
2016-09-17 20:15:56 -0400

    Main Menu Update
    Created the main menu scene with Andres, and provided base functionality
    for loading the main game.

---
+++ PlantWriteGrow/Assets/Scripts/User Interface/MenuScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;
+    }
+
+    public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene("Test");
+    }
+     
+    public void ExitGame()
+    {
+        Debug.Log("Bye");
+        Application.Quit();
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
