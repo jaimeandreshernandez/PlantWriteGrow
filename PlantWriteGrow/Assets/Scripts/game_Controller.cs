22989cfcc062c4ee6092b0f6b46d7053365da642
The Phi(Azn guy 2016) <g.poseidon@hotmail.com>
2016-09-18 12:31:44 -0400

    new words
    I added new words and i fixed the camera

--- PlantWriteGrow/Assets/Scripts/game_Controller.cs
+++ PlantWriteGrow/Assets/Scripts/game_Controller.cs
@@ -9,8 +9,17 @@ public class game_Controller : MonoBehaviour {
         "allo","whatever","young","money","perspective","computer","java","python","random",
 		"friend","stranger","why","common","laptop","success","professional","coffee","same","engine","camera","annoying","serialize","banana",
 		"system","verb","risk","collection","god","greek","america","when","who","typing","pooping","toilet","television","keyboard",
-		"graphic","music","piano","bottle","chair","control","project","tools","build","search","window","development","concrete"
-    };
+		"graphic","music","piano","bottle","chair","control","project","tools","build","search","window","development","concrete","appreciated","awesome","blissful",
+		"public","private","protected","default","package","void","main","constructor","mushroom","brilliant","cheerful","comfortable","courageous","inheritance","polymorphism",
+		"generic","delightful","dynamic","harmonious","assassination","minister","invigorated","irresistible","magnificent","marvellous","optimistic","passionate","peaceful",
+		"refreshed","sensational","spectacular","tranquil","unlimited","vivacious","welcomed","wonderful","yummy","abstract","appropriate","compound","content","coordinate","deliberate",
+		"duplicate","clone","interchange","miscount","perfect","predominate","exploding","swelling","rushing","relaxing","fluttering","bursting","disappearing","languorous","accelerating",
+		"tentative","bounding","resurrection","accustomed","admittance","advantageous","acquaintance","algorithm","ambidextrous","anorexia","apostrophe","cartography","cellulose","agglomeration",
+		"bankruptcy","characteristic","chromatography","chromosome","cinematographer","rosefish","mealtime","monarchy","propriety","fearfulness","farthermost","devoutness","agoraphobic","earthquake",
+		"whisper","road","rain","alarm","dog","cat","pig","cow","blind","you","me","jog","seashell","cheerleader","shopping","fiddle","stick","cart","car","bus","walk","run","jump","see","watch",
+		"country","four","five","six","sometimes","indian","might","hard","easy","between","exit","next","beginning","end","start","awake","mountain","family","lake","cave","feet","upper","lower",
+		"spring","hot","tube","sexuality","whore","both","those","while","wash","dry","tango","charlie","bravo"};
+	
 	private string _currentWord;
 	public Text txtCurrentWord;
 	private System.Random _rng = new System.Random();
