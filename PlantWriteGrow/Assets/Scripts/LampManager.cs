9be92b0d185bdadbad7f99bb61f53dffeed801cf
MatthewChrobak <ijustgoton@hotmail.com>
2016-09-18 04:14:33 -0400

    Lamp/Potato System
    Implemented a simple potato growth system based on the strength of the
    lamps.

---
+++ PlantWriteGrow/Assets/Scripts/LampManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System;
+
+public class LampManager : MonoBehaviour {
+
+    // Localize the light strength modification 
+    public static float LightStrength { private set; get; }
+
+    // The collection of light components contained in the lamp manager.
+    private Light[] _lamps;
+
+    // The next interval for degrading the lamps.
+    private int _tmrLampDecrementation;
+    public const int LAMP_DEGRADE_RATE = 100;
+    private const int MAX_LAMP_INTENSITY = 8;
+
+	// Use this for initialization
+	void Start () {
+        // Figure out how many lamps there are.
+        this._lamps = new Light[this.transform.childCount];
+
+        // Add them all.
+        for (int i = 0; i < this._lamps.Length; i++) {
+            var lampObject = this.transform.GetChild(i);
+
+            // Make sure that this lamp object has a child first.
+            if (lampObject.childCount > 0) {
+                this._lamps[i] = lampObject.GetChild(0).GetComponent<Light>();                
+            }
+        }
+
+        // Initialize the first value of the light strenght: 100%
+        LampManager.LightStrength = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Make sure we're not dead.
+        if (LampManager.LightStrength > 0) {
+            // Degrade the strength of the lights.
+            DegradeLamps();
+        }
+	}
+
+    public void UpgradeLamps() {
+        // Make sure we're not dead.
+        if (LampManager.LightStrength <= 0) {
+            return;
+        }
+
+        // Increment the light strengh, and update the lamps.
+        LampManager.LightStrength += 0.005f;
+        this.UpdateLampStrength();
+    }
+
+    private void DegradeLamps() {
+
+        // Get the current system tickcount.
+        int tick = Environment.TickCount;
+
+        // Can we degrade the lamps?
+        if (tick > this._tmrLampDecrementation) {
+            // Yes.
+            LampManager.LightStrength -= 0.01f;
+
+            //Debug.Log(LampManager.LightStrength);
+
+            // Update the lamps.
+            this.UpdateLampStrength();
+
+            // Set the time for the next degrade.
+            this._tmrLampDecrementation = tick + LampManager.LAMP_DEGRADE_RATE;
+        }
+    }
+
+    private void UpdateLampStrength() {
+        // Make sure we're not dead.
+        if (LampManager.LightStrength <= 0) {
+            return;
+        }
+
+        // Update the intensity.
+        foreach (var lamp in this._lamps) {
+            lamp.intensity = LampManager.LightStrength * (float)LampManager.MAX_LAMP_INTENSITY;
+        }
+    }
+}
---
+++ PlantWriteGrow/Assets/Scripts/PotatoManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PotatoManager : MonoBehaviour {
+
+    private Transform[] _potatoes;
+    public float StartYPos = -100;
+    private const float MAX_POTATO_SCALE = 2.0f;
+    private const float MID_FREEZING_POINT = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+        // Figure out how many child potatoes there are.
+        this._potatoes = new Transform[this.transform.childCount];
+
+        // Add them all.
+        for (int i = 0; i < this._potatoes.Length; i++) {
+            this._potatoes[i] = this.transform.GetChild(i);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        foreach (var child in _potatoes) {
+            float val = LampManager.LightStrength * PotatoManager.MAX_POTATO_SCALE;
+
+            // Find out if the potatoes are freezing or growing.
+            child.transform.localScale = new Vector3(val, val, val);
+        }
+
+        this.transform.localPosition = new Vector3(
+            this.transform.localPosition.x,
+            (1 - LampManager.LightStrength) * StartYPos,
+            this.transform.localPosition.z);
+	}
+}
